ce192888d0505532edcdd8777e17885112df9ebc
kbenjamin4 <kbenjamin4@student.pvamu.edu>
2019-07-18 17:01:01 -0500

    celsius folder

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheit/Source.cpp
@@ -0,0 +1,20 @@
+/*Write a program in C++ to convert temperature between Celsius and Fahrenheit.
+Conversion: C-->F = Multiply by 9, then divide by 5, then add 32
+F--->C = Deduct 32, then multiply by 5, then divide by 9*/
+
+//include statements
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//Main
+int main()
+{
+	//variable declarations
+	float Gtemp, A;
+	char measurement;
+	//ask for data
+	cout << "Enter and C for celsius or an F for fahrenheit then hit the space bar and enter the temperature";
+	//calculations
+
+}
